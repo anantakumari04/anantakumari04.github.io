20e870065cc73b6f0591f95f9b6e945c52a0f78f
anantakumari04 <kumariananta01@gmail.com>
2025-06-22 11:56:16 +0530

    js updated

--- js/file1.js
+++ js/file1.js
@@ -1,6 +1,37 @@
-let a = 10
+//Introduction to JavaScript
+// let studentName = "John"
+// let age = 20
+// let promoted = true
+// let score
+// let city = null
+// console.log(studentName)
+// console.log(typeof studentName)
+// console.log(typeof age)
+// console.log(typeof promoted)
+// console.log(typeof score)
+// console.log(typeof city)
 
-let b =20
+// let age = "10"
+// console.log(typeof age)
+// let ageNum = Number(age)
+// console.log(typeof ageNum)
 
-let c = a+b
-console.log(c)
+// let a = "10"
+// let b = 10
+// console.log(a+b)
+
+// let a = "10"
+// let b = 10
+// console.log(a*b)
+
+// const C1 = "LPU"
+// C1 = "MU"
+// console.log(C1)
+
+/*
+var stName = "John"
+console.log(stName)
+*/
+
+let stName = "Kushboo"
+console.log(`My name is ${stName}`)
---
+++ js/file9.js
@@ -0,0 +1,10 @@
+//arrays and methods
+// const score = [2, 1, 7, 5, 3];
+// console.log(score)
+// console.log(score[0])
+// score.push(9)
+// console.log(score)
+// console.log(score.length)
+// const newScore = [...score,9]
+// console.log(newScore)
+//shopping cart app
