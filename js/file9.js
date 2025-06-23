16764f4553149dd6c8b02b8d73bfa8146946c07d
anantakumari04 <kumariananta01@gmail.com>
2025-06-23 16:05:35 +0530

    shoppingcart updated

--- js/file9.js
+++ js/file9.js
@@ -1,10 +1,81 @@
-//arrays and methods
-// const score = [2, 1, 7, 5, 3];
+// //ARRAYS
+// const score =[2,1,7,5,3];
 // console.log(score)
-// console.log(score[0])
-// score.push(9)
-// console.log(score)
-// console.log(score.length)
-// const newScore = [...score,9]
+// console.log()
+// /////////////////////////////////////////////////////////////////////////
+
+// const cart ={
+
+//     1: 5,
+//     5: 3,
+// };
+// const newCart =({...cart,2:1})
+// console.log(newCart)
+// ///////////////////////////////////////////////////////////////////
+
+// const products=[
+//     { id: 1, name:"Product 1",price :25},
+//     { id: 2, name:"Product 2",price :50},
+//     { id: 3, name:"Product 3",price :75},
+// ];
+// const newProduct =[...product,{id: 4,name:"Product 4", price:56}]  // add new product
+// function addToCart(1){
+//     cart = {...cart,[id]:1};// id in square bracet because it will take id value
+// }
+// addToCart(1)
+// addToCart(2)
+// console.log(cart)
+// how we can decrease / increase the quantity
+
+const score = [2, 1, 7, 5, 3];
+
+//for each execute all the elements
+// score.forEach((a,b,c) => {
+//   console.log(c[b]);
+// });
+
+// score.forEach((value) => {
+//   console.log(value);
+// });
+
+// score.map((value) => {   //map used to create new array based on existing value
+//   console.log(value);
+// });
+
+// const newScore = score.map((value) => {
+//   return value+5
+//return value > 2 //this will returns as true false
+// });
+
 // console.log(newScore)
-//shopping cart app
+
+
+// const newScore = score.filter((value) => {
+//   return value > 2;
+// });
+
+// console.log(newScore)
+
+
+// const newScore = score.find((value) => {  //find only going to return first match i.e only 1 value
+//   return value > 2;
+// });
+
+// console.log(newScore)
+
+// const newScore = score.find((sum,value) =>  value==2);
+
+// console.log(newScore)
+
+
+// const newScore = score.reduce((sum,value) => {
+//   return sum+value;
+// },0);
+
+// console.log(newScore)
+
+
+
+
+
+
