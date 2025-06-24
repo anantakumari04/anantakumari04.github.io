f2cb6a859f862dbaaff44041163136fbad036221
anantakumari04 <kumariananta01@gmail.com>
2025-06-24 14:18:32 +0530

    updated

---
+++ js/file11.js
@@ -0,0 +1,11 @@
+//import export
+
+import add,{subtract} from "./calc.mjs"  //no default function then use , {function name}
+
+
+const result = add(4,5);
+console.log(result)
+
+const r1 = subtract(9,2)
+console.log(r1)
+
---
+++ js/file13.js
@@ -0,0 +1,17 @@
+//spread operator
+
+let marks = {
+    maths: 95,
+
+};
+
+marks = {...marks,science:97};
+
+console.log(marks)
+
+
+let students = ["Harsh","Ananta","Khusboo"]
+
+students = [...students, "Arindam"]
+
+console.log(students)
